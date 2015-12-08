b6b41439b503c5b9c303545d2edcd81bb7c6c942
Hugo Baeta <hugo@baeta.me>
2015-12-07 16:28:05 -0800

    more portfolio customization and footer contact styling

--- footer.php
+++ footer.php
@@ -14,12 +14,16 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-contact">
+            <p>Wanna talk? You can reach me by email at hugo(at)baeta(dot)me</p>
+            <p class="small">You can also check me out on <a href="http://twitter.com/hugobaeta/" title="@hugobaeta">Twitter</a>, <a href="http://linkedin.com/in/hugobaeta/" title="LinkedIn profile">LinkedIn</a>, or&nbsp;<a href="https://github.com/hugobaeta" title="Github profile">Github</a></p>
+        </div>
         <nav id="footer-navigation" class="main-navigation" role="navigation">
             <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
         </nav><!-- #footer-navigation -->
 		<ul class="site-info">
             <li class="cms-info">Proudly powered by <a href="http://wordpress.org/>">WordPress</a></li>
-            <li class="author-info">Made with Love by <a href="http://hugobaeta.com" rel="author">Hugo Baeta</a></li>            
+            <li class="author-info">Made with Love by <a href="http://hugobaeta.com" rel="author">Hugo Baeta</a></li>
         </ul><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
