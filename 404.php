c4e20e18246843613a928fd1a243032c685a1b21
Hugo Baeta <hugo@baeta.me>
2015-12-08 21:28:46 -0800

    Styled 404 template

--- 404.php
+++ 404.php
@@ -14,11 +14,11 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! Something went wrong here!', 'hugobaeta' ); ?></h1>
+					<h1 class="page-title">uh oh!</h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try a search?', 'hugobaeta' ); ?></p>
+					<p>Something went wrong here! It looks like nothing was found at this location. Maybe try a search?</p>
 
 					<?php get_search_form(); ?>
 
